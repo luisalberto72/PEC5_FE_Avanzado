fdef24a10d28ecae769b20d67a5af53d1b935dba
[Luis Alberto Mesa Martos] <[lmesamart@uoc.edu]>
2024-12-21 09:21:04 +0100

    Deploy Angular app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
+if (environment.production) {
+  enableProdMode();
+  
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/ngsw-worker.js').then(() => {
+      console.log('Service Worker registered successfully.');
+    }).catch(err => {
+      console.error('Service Worker registration failed:', err);
+    });
+  }
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
